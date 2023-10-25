be331da7fe1c30a76b1a539e9451521838c27c13
martinez-lopezpablo <133849651+martinez-lopezpablo@users.noreply.github.com>
2023-10-25 12:24:26 +0200

    Add files via upload

---
+++ step0_helloworld.m
@@ -0,0 +1,20 @@
+
+%Github: martinez-lopezpablo
+
+%In this script we complete step 0 of the course.
+%We try showing "Hello World" on the screen
+Screen('Preference','SkipSyncTests', 1); %This line avoids screen-frames issues 
+[window, rect]=Screen('OpenWindow',0);
+[screenWidth, screenHeight] = Screen('WindowSize', window);
+xCenter = (screenWidth)/ 2;
+yCenter = (screenHeight)/ 2;
+
+
+Screen('DrawText',window,'Hello world',xCenter,yCenter,1);
+Screen('Flip',window)
+WaitSecs(5)
+Screen('DrawText',window,'Goodbye world',xCenter,yCenter,1);
+Screen('Flip',window)
+WaitSecs(2)
+Screen('CloseAll');
+
