5d08e733c4c84268d5ea492014d9c10f962b6fa9
martinez-lopezpablo <133849651+martinez-lopezpablo@users.noreply.github.com>
2023-10-31 18:38:12 +0100

    step0_helloworld.m

--- step0_helloworld.m
+++ step0_helloworld.m
@@ -3,7 +3,7 @@
 
 %In this script we complete step 0 of the course.
 %We try showing "Hello World" on the screen
-Screen('Preference','SkipSyncTests', 1); %This line avoids screen-frames issues 
+Screen('Preference','SkipSyncTests', 1); %This line prevents screen-frames issues 
 [window, rect]=Screen('OpenWindow',0);
 [screenWidth, screenHeight] = Screen('WindowSize', window);
 xCenter = (screenWidth)/ 2;
