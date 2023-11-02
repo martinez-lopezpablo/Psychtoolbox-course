b575188fae99f265f24874e5e5222060ba47ef6c
martinez-lopezpablo <133849651+martinez-lopezpablo@users.noreply.github.com>
2023-11-02 19:26:07 +0100

    Update step0_helloworld.m

--- step0_helloworld.m
+++ step0_helloworld.m
@@ -1,21 +1,27 @@
-
 %Github: martinez-lopezpablo
+%Last update: 2/11/2023
 
 %In this script we complete step 0 of the course.
 %We try showing "Hello World" on the screen
+
 Screen('Preference','SkipSyncTests', 1); %This line prevents screen-frames issues 
-[window, rect]=Screen('OpenWindow',0);
-[screenWidth, screenHeight] = Screen('WindowSize', window);
-xCenter = (screenWidth)/ 2;
-yCenter = (screenHeight)/ 2;
 
+% Open a window for stimuli presentation
+[window, rect]= Screen('OpenWindow',0);
 
-Screen('DrawText',window,'Hello world',xCenter,yCenter,1);
-Screen('Flip',window)
-WaitSecs(5)
+% Define the center of the screen
+[xCenter, yCenter] = RectCenter(rect);
+
+%%Display the text "Hello world!"
+Screen('DrawText',window,'Hello world!',xCenter,yCenter,1);
+Screen('Flip',window);
+WaitSecs(5) %The text will be displayed for 5 seconds
+%Display the text "Goodbye world"
 Screen('DrawText',window,'Goodbye world',xCenter,yCenter,1);
-Screen('Flip',window)
-WaitSecs(2)
-Screen('CloseAll');
+Screen('Flip',window);
+WaitSecs(2) %The text will be displayed for 2 seconds
+%End of step 0 task
+Screen('CloseAll'); 
+
 %David: ¡Excelente! 
 
